2fd441949de9656b52866387f0e67b08ee0c9ce6
swapnil-maury <swapnilmaurya71@gmail.com>
2026-04-22 00:39:02 +0530

    Delete .ipynb_checkpoints directory

--- .ipynb_checkpoints/jupyter-checkpoint.ps1
+++
@@ -1,6 +0,0 @@
-if (Get-Command jupyter -ErrorAction SilentlyContinue) {
-    Write-Host "Starting Jupyter Notebook..."
-    jupyter notebook
-} else {
-    Write-Host "Jupyter is not installed or not in PATH." -ForegroundColor Red
-}
